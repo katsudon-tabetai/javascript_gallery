5926936dc52d8cb3e939d61353933e384427d655
katsudon-tabetai <131325371+katsudon-tabetai@users.noreply.github.com>
2024-10-15 10:32:36 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const main = document.querySelector('#main-image');
+const thumbs = document.querySelectorAll('.thumb');
+const text = document.querySelector('#caption');
+
+// 確認
+// console.log(main);
+// console.log(thumbs);
+
+thumbs.forEach(function(item, index) {
+    item.onmouseover = function() {
+        main.src = this.dataset.image;
+        text.textContent = this.title;
+    }
+});
